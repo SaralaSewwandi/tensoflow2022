8e2797a9f73480c05403a640deb1332e791e64f8
sarala <saralasewwandi@gmail.com>
2022-01-07 21:06:01 +0530

    perf counter added
    Signed-off-by: sarala <saralasewwandi@gmail.com>

--- load data using tf data - using list files.py
+++ load data using tf data - using list files.py
@@ -14,10 +14,11 @@ import pathlib
 import matplotlib.pyplot as plt
 import numpy as np
 import glob
+import time
 
 # data_dir="E:\\flower_photos"
 # data_dir = pathlib.Path(data_dir)
-
+start_time = time.perf_counter()
 #Download the image data set from the cloud -  vedio/blog content 1
 
 #1.define the image data set url
@@ -243,6 +244,6 @@ model.fit(
   epochs=3
 )
 
-
-
+print("Execution time:", time.perf_counter() - start_time)
+#Execution time: 262.6251387
 
