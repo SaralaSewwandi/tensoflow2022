0d4e6f7360dac16b3b63e814ac17ca4b1472c19b
sarala <saralasewwandi@gmail.com>
2022-01-11 11:56:03 +0530

    Trained a model for MNIST without pruning evaluated the test accuracy
    Signed-off-by: sarala <saralasewwandi@gmail.com>

---
+++ Model Optimization/Pruning.py
@@ -0,0 +1,49 @@
+import tempfile
+import os
+
+import tensorflow as tf
+import numpy as np
+
+from tensorflow import keras
+'''
+1.Train a model for MNIST without pruning
+'''
+# Load MNIST dataset
+mnist = keras.datasets.mnist
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+# Normalize the input image so that each pixel value is between 0 and 1.
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+# Define the model architecture.
+model = keras.Sequential([
+  keras.layers.InputLayer(input_shape=(28, 28)),
+  keras.layers.Reshape(target_shape=(28, 28, 1)),
+  keras.layers.Conv2D(filters=12, kernel_size=(3, 3), activation='relu'),
+  keras.layers.MaxPooling2D(pool_size=(2, 2)),
+  keras.layers.Flatten(),
+  keras.layers.Dense(10)
+])
+
+# Train the digit classification model
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.fit(
+  train_images,
+  train_labels,
+  epochs=4,
+  validation_split=0.1,
+)
+
+#Evaluate baseline test accuracy and save the model for later usage.
+_, baseline_model_accuracy = model.evaluate(
+    test_images, test_labels, verbose=0)
+
+print('Baseline test accuracy:', baseline_model_accuracy)
+
+_, keras_file = tempfile.mkstemp('.h5')
+tf.keras.models.save_model(model, keras_file, include_optimizer=False)
+print('Saved baseline model to:', keras_file)
