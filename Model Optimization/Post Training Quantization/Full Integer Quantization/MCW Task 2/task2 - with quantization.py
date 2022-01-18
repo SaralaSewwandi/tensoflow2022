e46d02134b8cec6c2a6d86dcd5dd8e681c2ed410
sarala <saralasewwandi@gmail.com>
2022-01-18 16:03:50 +0530

    mcw task 2 developed tensorflow inference pipeline for the evaluation using resnet model trained on imagenet data set
    Signed-off-by: sarala <saralasewwandi@gmail.com>

---
+++ Model Optimization/Post Training Quantization/Full Integer Quantization/MCW Task 2/task2 - with quantization.py
@@ -0,0 +1,167 @@
+#Setup
+#In order to quantize both the input and output tensors, we need to use APIs added in TensorFlow r2.3:
+
+
+import logging
+logging.getLogger("tensorflow").setLevel(logging.DEBUG)
+
+import tensorflow as tf
+import numpy as np
+import tensorflow_hub as hub
+assert float(tf.__version__[:3]) >= 2.3
+
+'''
+Generate a TensorFlow Model
+We'll build a simple model to classify numbers from the MNIST dataset.
+
+This training won't take long because you're training the model for just a 5 epochs, which trains to about ~98% accuracy.
+'''
+
+# Load MNIST dataset
+mnist = tf.keras.datasets.mnist
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+# Normalize the input image so that each pixel value is between 0 to 1.
+train_images = train_images.astype(np.float32) / 255.0
+test_images = test_images.astype(np.float32) / 255.0
+
+# Define the model architecture
+model = tf.keras.Sequential([
+  tf.keras.layers.InputLayer(input_shape=(224, 224, 3)),
+  hub.KerasLayer("https://tfhub.dev/google/imagenet/resnet_v2_101/classification/4")
+])
+
+
+'''
+Convert using integer-only quantization
+To quantize the variable data (such as model input/output and intermediates between layers), you need to provide a RepresentativeDataset. 
+This is a generator function that provides a set of input data that's large enough to represent typical values. It allows the converter to estimate a dynamic range for all the variable data. 
+(The dataset does not need to be unique compared to the training or evaluation dataset.) To support multiple inputs, each representative data point is a list and elements in the list are fed to the model according to their indices.
+
+'''
+
+def representative_data_gen():
+  for input_value in tf.data.Dataset.from_tensor_slices(train_images).batch(1).take(100):
+    yield [input_value]
+
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+converter.representative_dataset = representative_data_gen
+# Ensure that if any ops can't be quantized, the converter throws an error
+converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+# Set the input and output tensors to uint8 (APIs added in r2.3)
+converter.inference_input_type = tf.uint8
+converter.inference_output_type = tf.uint8
+
+tflite_model_quant = converter.convert()
+
+#The internal quantization remains the same as float fallack quantization, but you can see the input and output tensors are now integer format:
+interpreter = tf.lite.Interpreter(model_content=tflite_model_quant)
+input_type = interpreter.get_input_details()[0]['dtype']
+print('input: ', input_type)
+output_type = interpreter.get_output_details()[0]['dtype']
+print('output: ', output_type)
+
+#Now you have an integer quantized model that uses integer data for the model's input and output tensors, so it's compatible with integer-only hardware such as the Edge TPU.
+
+'''
+Save the models as files
+You'll need a .tflite file to deploy your model on other devices.
+So let's save the converted models to files and then load them when we run inferences below.
+'''
+import pathlib
+
+tflite_models_dir = pathlib.Path("/tmp/resnet_tflite_models/full_integer_quantization")
+tflite_models_dir.mkdir(exist_ok=True, parents=True)
+
+# Save the quantized model:
+tflite_model_quant_file = tflite_models_dir/"resnet_model_quant_integer_only.tflite"
+tflite_model_quant_file.write_bytes(tflite_model_quant)
+
+
+'''
+Run the TensorFlow Lite models
+Now we'll run inferences using the TensorFlow Lite Interpreter to compare the model accuracies.
+
+First, we need a function that runs inference with a given model and images, and then returns the predictions:
+'''
+
+# Helper function to run inference on a TFLite model
+def run_tflite_model(tflite_file, test_image_indices):
+  global test_images
+
+  # Initialize the interpreter
+  interpreter = tf.lite.Interpreter(model_path=str(tflite_file))
+  interpreter.allocate_tensors()
+
+  input_details = interpreter.get_input_details()[0]
+  output_details = interpreter.get_output_details()[0]
+
+  predictions = np.zeros((len(test_image_indices),), dtype=int)
+  for i, test_image_index in enumerate(test_image_indices):
+    test_image = test_images[test_image_index]
+    test_label = test_labels[test_image_index]
+
+    # Check if the input type is quantized, then rescale input data to uint8
+    if input_details['dtype'] == np.uint8:
+      input_scale, input_zero_point = input_details["quantization"]
+      test_image = test_image / input_scale + input_zero_point
+
+    test_image = np.expand_dims(test_image, axis=0).astype(input_details["dtype"])
+    interpreter.set_tensor(input_details["index"], test_image)
+    interpreter.invoke()
+    output = interpreter.get_tensor(output_details["index"])[0]
+
+    predictions[i] = output.argmax()
+
+  return predictions
+
+
+'''
+Test the models on one image
+Now we'll compare the performance of the float model and quantized model:
+
+tflite_model_file is the original TensorFlow Lite model with floating-point data.
+tflite_model_quant_file is the last model we converted using integer-only quantization (it uses uint8 data for input and output).
+'''
+
+#Let's create another function to print our predictions:
+import matplotlib.pylab as plt
+
+# Change this to test a different image
+test_image_index = 1
+
+## Helper function to test the models on one image
+def test_model(tflite_file, test_image_index, model_type):
+  global test_labels
+
+  predictions = run_tflite_model(tflite_file, [test_image_index])
+
+  plt.imshow(test_images[test_image_index])
+  template = model_type + " Model \n True:{true}, Predicted:{predict}"
+  _ = plt.title(template.format(true= str(test_labels[test_image_index]), predict=str(predictions[0])))
+  plt.grid(False)
+  plt.show()
+
+test_model(tflite_model_quant_file, test_image_index, model_type="Quantized")
+
+'''
+Evaluate the models on all images
+Now let's run both models using all the test images we loaded at the beginning of this tutorial:
+'''
+
+# Helper function to evaluate a TFLite model on all images
+def evaluate_model(tflite_file, model_type):
+  global test_images
+  global test_labels
+
+  test_image_indices = range(test_images.shape[0])
+  predictions = run_tflite_model(tflite_file, test_image_indices)
+
+  accuracy = (np.sum(test_labels== predictions) * 100) / len(test_images)
+
+  print('%s model accuracy is %.4f%% (Number of test samples=%d)' % (
+      model_type, accuracy, len(test_images)))
+
+evaluate_model(tflite_model_quant_file, model_type="Quantized")
+
